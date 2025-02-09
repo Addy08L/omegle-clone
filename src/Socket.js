d297960671f2a53c1b62b0b740964d8b76babd60
Addy08L <adityalanjewar803@gmail.com>
2025-02-09 10:47:54 +0530

    Update Socket.js

--- src/Socket.js
+++ src/Socket.js
@@ -1,103 +1,66 @@
 import { useEffect } from 'react';
-import { io } from 'socket.io-client';
 import { v4 as uuidv4 } from 'uuid';
 import { useChat } from './contextApi/ChatContext';
-// import beepSound from "./assets/ping-82822.mp3"
 
-const URL = process.env.REACT_APP_BASE_URL
+// Replace this with your Railway WebSocket server URL
+const URL = "wss://qmegle-signaling.up.railway.app";
 
-export const socket = io(URL, {
-    autoConnect: false,
-    reconnectionAttempts: 3
-});
+export const socket = new WebSocket(URL);
 
 const Socket = () => {
-    const { setUserId, setIsConnected, setMessages, setOnlineUsers, receiver, setReceiver, setIsSearching, setIsTyping, setMessage, setIsSending } = useChat()
+    const { setUserId, setIsConnected, setMessages, setOnlineUsers, setReceiver, setIsSearching, setIsTyping, setMessage, setIsSending } = useChat();
 
     useEffect(() => {
-        socket.connect();
-        console.log("socket connected");
-        return () => {
-            socket.disconnect();
-            console.log("socket disconnected");
-        };
-    }, []);
-
-    useEffect(() => {
-        function onConnect() {
+        socket.onopen = () => {
+            console.log("Connected to WebSocket signaling server");
             setIsConnected(true);
-        }
+        };
 
-        function onDisconnect() {
+        socket.onclose = () => {
+            console.log("Disconnected from WebSocket signaling server");
             setIsConnected(false);
-        }
-
-        socket.on("connect", onConnect);
-        socket.on("disconnect", onDisconnect);
+        };
 
         return () => {
-            socket.off("connect", onConnect);
-            socket.off("disconnect", onDisconnect);
+            socket.close();
         };
     }, [setIsConnected]);
 
     useEffect(() => {
         const uniqueId = uuidv4();
-        setUserId(uniqueId)
-        socket.emit("new-online-user", uniqueId, (error) => {
-            if (error) {
-                return alert(error);
+        setUserId(uniqueId);
+        
+        socket.send(JSON.stringify({ type: "new-online-user", userId: uniqueId }));
+
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            
+            if (data.type === "get-online-users") {
+                setOnlineUsers(data.users);
+            } else if (data.type === "send-message") {
+                setMessages((previous) => [...previous, { stranger: data.message }]);
+                setIsTyping(false);
+            } else if (data.type === "receive-message") {
+                setMessages((previous) => [...previous, { you: data.message }]);
+                setIsSending(false);
+            } else if (data.type === "user-paired") {
+                setReceiver(data.receiver);
+                setIsSearching(false);
+            } else if (data.type === "chat-close") {
+                setReceiver("");
+                setMessage("");
+                setIsTyping(false);
+            } else if (data.type === "typing") {
+                setIsTyping(true);
+            } else if (data.type === "typing stop") {
+                setIsTyping(false);
             }
-        });
-
-        socket.on("get-online-users", (users) => {
-            setOnlineUsers(users);
-        });
-
-        socket.on("send-message", (message) => {
-            setMessages((previous) => [
-                ...previous,
-                { stranger: message },
-            ]);
-            setIsTyping(false)
-        });
-
-        socket.on("receive-message", (message) => {
-            setMessages((previous) => [
-                ...previous,
-                { you: message },
-            ]);
-            setIsSending(false)
-        });
-
-        socket.on("user-paired", (receiver) => {
-            setReceiver(receiver)
-            // const audio = new Audio(beepSound);
-            // audio.play();
-            setIsSearching(false)
-        })
-
-        socket.on("chat-close", () => {
-            setReceiver("")
-            setMessage("")
-            setIsTyping(false)
-        })
-
-        socket.on("typing", () => {
-            setIsTyping(true)
-        })
-
-        socket.on("typing stop", () => {
-            setIsTyping(false)
-        })
+        };
 
         return () => {
-            socket.off("get-online-users");
-            socket.off("send-message");
-            socket.off("new-online-user");
-            socket.off("user-paired")
+            socket.onmessage = null;
         };
-    }, []);
-}
+    }, [setUserId, setOnlineUsers, setMessages, setReceiver, setIsSearching, setIsTyping, setMessage, setIsSending]);
+};
 
-export default Socket
+export default Socket;
